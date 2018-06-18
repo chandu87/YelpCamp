5a594fc8e2819c6c7592247afdbfd5264f4a5be7
chandu <chandrasr.palle@gmail.com>
2018-06-19 00:42:34 +0200

    campgrounds page added

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/campgrounds", function(req, res){
+    const campData = [
+                    {name : "Tissvilde", image : ""},
+                    {name : "Mons-Klint", image : ""},
+                    {name : "Klampenborg", image : ""}];
+    res.render("campgrounds", {data : campData});
+});
+
 app.listen(3000, function(){
     console.log("Server started at : 3000");
 });
