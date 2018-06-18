dcb409f2f4a32198b9f6ba370242e5dc0a535097
chandu <chandrasr.palle@gmail.com>
2018-06-19 00:48:18 +0200

    images added for campgrounds route

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
     const campData = [
-                    {name : "Tissvilde", image : ""},
-                    {name : "Mons-Klint", image : ""},
-                    {name : "Klampenborg", image : ""}];
+                    {name : "Tissvilde", image : "http://www.photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104497f9c27ca7efb1bd_960.jpg&user=Pexels"},
+                    {name : "Mons-Klint", image : "http://www.photosforclass.com/download/pixabay-2768515?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Feb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104497f9c27ca7efb1bd_960.jpg&user=rawpixel"},
+                    {name : "Klampenborg", image : "http://www.photosforclass.com/download/flickr-14435096036"}];
     res.render("campgrounds", {data : campData});
 });
 
