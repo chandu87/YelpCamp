8de8acd18113e3ec5fec65e2c9cb8be0d055c8b2
chandu <chandrasr.palle@gmail.com>
2018-07-05 08:17:45 +0200

    comment schema changed , adde user info

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,12 @@
 const mongoos = require('mongoose');
 const commentSchema = mongoos.Schema({
    text: String,
-   author: String 
+   author: {
+       id: {
+           type: mongoos.Schema.Types.ObjectId,
+           ref: "User"
+       },
+       username: String
+   }
 });
 module.exports = mongoos.model("Comment", commentSchema);
